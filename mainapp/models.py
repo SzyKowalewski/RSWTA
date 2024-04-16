cfe5865e53eefc4a46e0f222c7993938463e6c8b
Zormufark <ttoommkul@gmail.com>
2024-04-16 13:13:50 +0200

    parametry do recenzji

--- mainapp/models.py
+++ mainapp/models.py
@@ -30,8 +30,8 @@ class Do_obejrzenia(models.Model): # NOQA
 class Rezencje(models.Model): # NOQA
     Ocena = models.IntegerField() # NOQA
     Komentarz = models.TextField() # NOQA
-    ID_Uzytkownika = models.ForeignKey()
-    ID_Produkcji = models.ForeignKey()
+    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE) # NOQA
+    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE) # NOQA
 
     def __str__(self):
         return self.Komentarz
