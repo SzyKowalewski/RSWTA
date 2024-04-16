6e70552b0d8d7bcf59250ee5b7e1264d988d64be
pingwin01Games <herman-marcel@wp.pl>
2024-04-16 11:51:09 +0200

    Models part 2

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,3 +15,25 @@ class Do_obejrzenia(models.Model):
     ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE)
     ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
 
+class Rezencje(models.Model):
+    Ocena = models.IntegerField()
+    Komentarz = models.TextField()
+    ID_Uzytkownika = models.ForeignKey()
+    ID_Produkcji = models.ForeignKey()
+
+    def __str__(self):
+        return self.Komentarz
+
+
+class Uzytkownicy(models.Model):
+    Nazwa_Uzytkownika = models.TextField()
+    Haslo = models.TextField()
+    E_mail = models.TextField()
+    Publiczne = models.BooleanField()
+
+    def __str__(self):
+        return self.Nazwa_Uzytkownika
+
+class Obejrzane(models.Model):
+    ID_Produkcji = models.ForeignKey()
+    ID_Uzytkownika = models.ForeignKey()
