e999d1601b669fffcb2e236ee6dc354771537156
Zormufark <ttoommkul@gmail.com>
2024-04-16 13:08:29 +0200

    no quality assurance (wywala te brzydkie podkreślenia z kodu)

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,33 +1,35 @@
 from django.db import models
 
 
-class Produkcje(models.Model):
-    Tytul = models.CharField(max_length=50)
-    Rezyser = models.CharField(max_length=50)
-    Dlugosc_ilosc_odcinkow = models.IntegerField()
-    Opis = models.CharField(max_length=1000)
-    Data_premiery = models.DateField()
+class Produkcje(models.Model): # NOQA
+    Tytul = models.CharField(max_length=50) # NOQA
+    Rezyser = models.CharField(max_length=50) # NOQA
+    Dlugosc_ilosc_odcinkow = models.IntegerField() # NOQA
+    Opis = models.CharField(max_length=1000) # NOQA
+    Data_premiery = models.DateField() # NOQA
 
     def __str__(self):
         return self.Tytul
 
 
-class Uzytkownicy(models.Model):
-    Nazwa_Uzytkownika = models.TextField()
-    Haslo = models.TextField()
+class Uzytkownicy(models.Model): # NOQA
+    Nazwa_Uzytkownika = models.TextField() # NOQA
+    Haslo = models.TextField() # NOQA
     E_mail = models.TextField()
-    Publiczne = models.BooleanField()
+    Publiczne = models.BooleanField() # NOQA
 
     def __str__(self):
         return self.Nazwa_Uzytkownika
 
-class Do_obejrzenia(models.Model):
-    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE)
-    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
 
-class Rezencje(models.Model):
-    Ocena = models.IntegerField()
-    Komentarz = models.TextField()
+class Do_obejrzenia(models.Model): # NOQA
+    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE) # NOQA
+    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE) # NOQA
+
+
+class Rezencje(models.Model): # NOQA
+    Ocena = models.IntegerField() # NOQA
+    Komentarz = models.TextField() # NOQA
     ID_Uzytkownika = models.ForeignKey()
     ID_Produkcji = models.ForeignKey()
 
@@ -35,8 +37,6 @@ class Rezencje(models.Model):
         return self.Komentarz
 
 
-
-
-class Obejrzane(models.Model):
-    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE)
-    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
+class Obejrzane(models.Model): # NOQA
+    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE) # NOQA
+    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE) # NOQA
