db75871cd83c178d9e16535b25fb73ffffe3f626
Szymon Kowalewski <kowalewski2002@gmail.com>
2024-04-16 11:48:13 +0200

    model prod i do_ob

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Produkcje(models.Model):
+    Tytul = models.CharField(max_length=50)
+    Rezyser = models.CharField(max_length=50)
+    Dlugosc_ilosc_odcinkow = models.IntegerField()
+    Opis = models.CharField(max_length=1000)
+    Data_premiery = models.DateField()
+
+    def __str__(self):
+        return self.Tytul
+
+class Do_obejrzenia(models.Model):
+    ID_Produkcji = models.ForeignKey(Produkcje, on_delete=models.CASCADE)
+    ID_Uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE)
+
