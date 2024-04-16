e35c1c7724fab9758802a1f9873650495000340d
Szymon Kowalewski <kowalewski2002@gmail.com>
2024-04-16 17:38:37 +0200

    Panel admina

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produkcje
+
+admin.site.register(Produkcje)
+
+from .models import Uzytkownicy
+
+admin.site.register(Uzytkownicy)
+
+from .models import Do_obejrzenia
+
+admin.site.register(Do_obejrzenia)
+
+from .models import Rezencje
+
+admin.site.register(Rezencje)
+
+from .models import Obejrzane
+
+admin.site.register(Obejrzane)
