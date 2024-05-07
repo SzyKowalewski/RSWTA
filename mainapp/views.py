6fa84e62d92c8e3ca3b04a7dfc6a2989ba7c942b
Szymon Kowalewski <kowalewski2002@gmail.com>
2024-05-07 23:52:23 +0200

    Filtrowanie po kategoriach i po frazie

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,11 +4,22 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.views import generic
 from .models import Produkcje, Kategorie
+from django.db.models import Q
 
 
 def widok_home(request):
     kategorie = Kategorie.objects.all()
     filmy = Produkcje.objects.all()
+
+    wybrana_kategoria = request.GET.get('kategoria')
+    wpisana_fraza = request.GET.get('fraza')
+
+    if wybrana_kategoria:
+        filmy = filmy.filter(ID_Kategorii=wybrana_kategoria)
+
+    if wpisana_fraza:
+        filmy = filmy.filter(Q(Tytul__icontains=wpisana_fraza) | Q(Rezyser__icontains=wpisana_fraza))
+
     return render(request, 'home.html', {'filmy': filmy, 'kategorie': kategorie})
 
 
