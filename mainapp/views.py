e9d95e001abe417ec06fc570e2e54d7d33e102d1
Zormufark <ttoommkul@gmail.com>
2024-05-07 23:25:49 +0200

    drobne zmiany ze stroną główną

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views import generic
-from .models import Produkcje
+from .models import Produkcje, Kategorie
 
-class HomeView(generic.base.TemplateView):
-   template_name = "home.html"
+
+# class HomeView(generic.base.TemplateView):
+#    template_name = "home.html"
+
+
 def widok_home(request):
+    kategorie = Kategorie.objects.all()
     filmy = Produkcje.objects.all()
-    return render(request, 'home.html', {'filmy': filmy})
+    return render(request, 'home.html', {'filmy': filmy, 'kategorie': kategorie})
+
 
 class BaseView(generic.base.TemplateView):
     template_name = "base.html"
