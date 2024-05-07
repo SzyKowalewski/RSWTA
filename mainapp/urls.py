e2f0d77fecc6b600ea5b3e9cea03af84b57bf1bc
Szymon Kowalewski <kowalewski2002@gmail.com>
2024-05-07 15:33:19 +0200

    Chyba działa łaczenie z bazą

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+from .views import widok_home
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
+    path("", widok_home, name="home"),
     path('admin/', admin.site.urls),
     path('RSWTA/', views.BaseView.as_view(), name='RSWTA')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views import generic
-
+from .models import Produkcje
 
 class HomeView(generic.base.TemplateView):
-    template_name = "home.html"
-
+   template_name = "home.html"
+def widok_home(request):
+    filmy = Produkcje.objects.all()
+    return render(request, 'home.html', {'filmy': filmy})
 
 class BaseView(generic.base.TemplateView):
     template_name = "base.html"
